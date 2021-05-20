442e5b88c45c99c456ac8fb4f54e0842865c6dd0
Wameedh Mohammed Ali <wameedh.wf@gmail.com>
2021-05-20 01:21:05 -0700

    test-11

--- SelfDrivingCar.c
+++ SelfDrivingCar.c
@@ -51,9 +51,9 @@ int main()
     PCA9685_SetPWMFreq(500);
 
     // creating thread that would be scanning for obstacles 
-    error0 = pthread_create(&left_ls_thread_id, NULL, sensingTheLeftLine, NULL);
-    // creating thread that would be sensing for the line 
-    error1 = pthread_create(&right_ls_thread_id, NULL, sensingTheRightLine, NULL);
+    // error0 = pthread_create(&left_ls_thread_id, NULL, sensingTheLeftLine, NULL);
+    // // creating thread that would be sensing for the line 
+    // error1 = pthread_create(&right_ls_thread_id, NULL, sensingTheRightLine, NULL);
 
     if (error0 || error1) {
          printf("Error: unable to create thread/s \n");
